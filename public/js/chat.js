7aea7cab43b078bea450253e9b4bfce32ab69852
Sujeev-Uthayakumar <sujeev115115@gmail.com>
2021-09-24 10:50:11 -0400

    refactored form

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,13 +1,26 @@
 const socket = io();
 
+// Elements
+const $messageForm = document.querySelector("#message-form");
+const $messageFormInput = $messageForm.querySelector("input");
+const $messageFormButton = $messageForm.querySelector("button");
+const $weatherFormButton = document.querySelector("#send-location");
+
 socket.on("message", (message) => {
   console.log(message);
 });
 
-document.querySelector("#messageSend").addEventListener("click", (e) => {
+$messageForm.addEventListener("submit", (e) => {
   e.preventDefault();
+
+  $messageFormButton.setAttribute("disabled", "disabled");
+
   const message = document.querySelector("#messageBox").value;
   socket.emit("sendMessage", message, (error) => {
+    $messageFormButton.removeAttribute("disabled");
+    $messageFormInput.value = "";
+    $messageFormInput.focus();
+
     if (error) {
       return console.log(error);
     }
@@ -15,10 +28,12 @@ document.querySelector("#messageSend").addEventListener("click", (e) => {
   });
 });
 
-document.querySelector("#send-location").addEventListener("click", (e) => {
+$weatherFormButton.addEventListener("click", (e) => {
   if (!navigator.geolocation) {
     return alert("Geolocation is not supported by your browser");
   }
+
+  $weatherFormButton.setAttribute("disabled", "disabled");
 
   navigator.geolocation.getCurrentPosition((position) => {
     socket.emit(
@@ -28,6 +43,8 @@ document.querySelector("#send-location").addEventListener("click", (e) => {
         longitude: position.coords.longitude,
       },
       () => {
+        $weatherFormButton.removeAttribute("disabled");
+
         console.log("Location shared!");
       }
     );
