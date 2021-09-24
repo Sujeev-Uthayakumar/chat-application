b585335840d28f46f7aeb1ed57d76f271173369c
sujeev-uthayakumar <sujeev115115@gmail.com>
2021-09-24 03:11:48 -0400

    added user messages

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,7 +4,7 @@ socket.on("message", (message) => {
   console.log(message);
 });
 
-document.querySelector("#messageSend").addEventListener("submit", (e) => {
+document.querySelector("#messageSend").addEventListener("click", (e) => {
   e.preventDefault();
   const message = document.querySelector("#messageBox").value;
   socket.emit("sendMessage", message);
