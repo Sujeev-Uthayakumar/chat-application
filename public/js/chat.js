8f764acf8cf3ac3868a0d4a7985486a8c2d80965
Sujeev-Uthayakumar <sujeev115115@gmail.com>
2021-09-26 01:44:16 -0400

    added socket.io rooms

--- public/js/chat.js
+++ public/js/chat.js
@@ -10,19 +10,35 @@ const $messages = document.querySelector("#messages");
 // Templates
 const messageTemplate = document.querySelector("#message-template").innerHTML;
 const locationTemplate = document.querySelector("#location-template").innerHTML;
+const sidebarTemplate = document.querySelector("#sidebar-template").innerHTML;
+
+// Options
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true,
+});
 
 socket.on("message", (message) => {
   console.log(message);
   const html = Mustache.render(messageTemplate, {
+    username: message.username,
     message: message.text,
     createdAt: moment(message.createdAt).format("h:mm a"),
   });
   $messages.insertAdjacentHTML("beforeend", html);
 });
 
+socket.on("roomData", ({ room, users }) => {
+  const html = Mustache.render(sidebarTemplate, {
+    room,
+    users,
+  });
+  document.querySelector("#sidebar").innerHTML = html;
+});
+
 socket.on("locationMessage", (message) => {
   console.log(message);
   const html = Mustache.render(locationTemplate, {
+    username: message.username,
     message: message.url,
     createdAt: moment(message.createdAt).format("h:mm a"),
   });
@@ -68,4 +84,11 @@ $weatherFormButton.addEventListener("click", (e) => {
       }
     );
   });
+});
+
+socket.emit("join", { username, room }, (error) => {
+  if (error) {
+    alert(error);
+    location.href = "/";
+  }
 });
--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,12 +1,14 @@
-const generateMessage = (text) => {
+const generateMessage = (username, text) => {
   return {
+    username,
     text,
     createdAt: new Date().getTime(),
   };
 };
 
-const generateLocationMessage = (weather) => {
+const generateLocationMessage = (username, weather) => {
   return {
+    username,
     url: weather,
     createdAt: new Date().getTime(),
   };
